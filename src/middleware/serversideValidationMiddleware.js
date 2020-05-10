7716f69206009b4a45f58bbf66575311fd794e55
Enzo Hilzinger <enzo.hilzinger@sap.com>
2020-05-10 20:24:34 +0200

    fixed missing handover

--- src/middleware/serversideValidationMiddleware.js
+++ src/middleware/serversideValidationMiddleware.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 
 module.exports = function serversideValidationMiddleware(req, res, next) {
-    if (!process.env.MONGODB_CONN_STRING) res.status(501).json({ error: "Database Credentials Not Set, No Query Possible" });
+    if (!process.env.MONGODB_CONN_STRING) res.status(501).json({ error: "Database Credentials Not Set, No Query Possible", description: "Please set the 'MONGODB_CONN_STRING' environment variable." });
+    next();
 }
